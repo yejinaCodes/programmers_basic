da94ff9f732b2fcd72bf6478b52573af7a950464
YejinKimHanyang <yejinkim@hanyang.ac.kr>
2024-06-17 17:40:35 +0900

    changes

--- src/practice13_samhangOperator.java
+++ src/practice13_samhangOperator.java
@@ -1,6 +1,5 @@
 import java.util.Calendar;
 public class practice13_samhangOperator {
-    public class TernaryExam {
         public static void main(String[] args) {
             // hour에는 현재 시간이 24시간 단위로 들어 있습니다.
             int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
